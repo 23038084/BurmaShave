ddb929e496202d4f96e6f20ab10cd6ab9f03546c
23038084 <163391231+23038084@users.noreply.github.com>
2025-04-04 10:47:51 +0200

    Add files via upload

---
+++ BurmaShaveJava.java/src/burmashavejava/java/BurmaShaveJavaJava.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package burmashavejava.java;
+
+/**
+ *
+ * @author pamel
+ */
+public class BurmaShaveJavaJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Shaving brushes");
+        System.out.println("You'll soon see 'em");
+        System.out.println("On shelf");
+        System.out.println("In some museum");
+       
+    }
+    
+}
